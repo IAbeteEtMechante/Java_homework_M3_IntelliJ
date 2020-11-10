2bc35a605851060f370ad1f8d25236585676a685
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-10 11:21:09 +0700

    Create project and add files Homework1.java, and Task1.java, and input.txt.correct the syntax to compile.

---
+++ src/main/java/HW01/Homework1.java
@@ -0,0 +1,41 @@
+package HW01;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Homework1 {
+    private static int[] readFile() {
+        try {
+            Scanner sc = new Scanner(new File("src/main/java/hw1/input.txt"));
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            return new int[]{a, b};
+        } catch (FileNotFoundException ex) {
+            return null;
+        }
+    }
+
+    private static void writeFile(int number) {
+        try {
+            Writer writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream("src/main/java/hw1/output.txt"), "ascii"));
+            writer.write(Integer.valueOf(number).toString());
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("IO Exception error.");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] vals = readFile();
+        if (vals == null)
+            System.out.println("You didn't provide an input.txt file, please add an input.txt file to this folder and retry.'");
+        else {
+            long mult = vals[0] * vals[1];
+            writeFile( (int) mult);
+        }
+    }
+}
+
